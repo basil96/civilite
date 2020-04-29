7fc57f3ff633169c9aec4a33ad3df7140e06e127
scliddle <63318293+scliddle@users.noreply.github.com>
2020-04-29 00:07:44 -0400

    cleanup some minor issues

--- src/setup.py
+++ src/setup.py
@@ -13,25 +13,26 @@ Install a link for development work (from ./src directory)
 """
 
 import os
-
+from importlib.util import module_from_spec, spec_from_file_location
 from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
+SPEC = spec_from_file_location("meta", "./civilite/_meta.py")
+METADATA = module_from_spec(SPEC)
+SPEC.loader.exec_module(METADATA)
+__version__ = METADATA.__version__
+__author__ = METADATA.__author__
+__author_email__ = METADATA.__author_email__
+__license__ = METADATA.__license__
+__url__ = METADATA.__url__
+
+ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
 try:
-    with open(os.path.join(here, 'README.md')) as f:
+    with open(os.path.join(ROOT_PATH, 'README.md')) as f:
         README = f.read()
 except IOError:
     README = ''
 
-from importlib.util import module_from_spec, spec_from_file_location
-spec = spec_from_file_location("meta", "./civilite/_meta.py") 
-meta = module_from_spec(spec)
-spec.loader.exec_module(meta)
-__version__ = meta.__version__
-__author__ = meta.__author__
-__author_email__ = meta.__author_email__
-__license__ = meta.__license__
-__url__ = meta.__url__
+
 
 
 setup(
@@ -42,7 +43,8 @@ setup(
     package_dir={},
     # metadata
     version=__version__,
-    description="A Python Library for managing outdoor lighting when building occupancy and available daylight must be considered.",
+    description="A Python Library for managing outdoor lighting when building occupancy and"
+                " available daylight must be considered.",
     long_description=README,
     author=__author__,
     author_email=__author_email__,
@@ -60,9 +62,12 @@ setup(
         'Issues': 'https://github.com/basil96/civilite/issues',
     },
     classifiers=[
-                 'Operating System :: Microsoft :: Windows',
-                 'License :: OSI Approved :: MIT License',
-                 'Programming Language :: Python',
-                 'Programming Language :: Python :: 3.7'
-                 ],
-    )
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Operating System :: Microsoft :: Windows',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+)
